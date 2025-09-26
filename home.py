173fda17ce8510512931c9e89eb5998bf31ae774
sadhvichaurasiya293 <sadhvichaurasiya293@gmail.com>
2025-09-26 11:11:49 +0530

    first commit

---
+++ home.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+st.title("Welcome to the Home Page")
+
+st.image("spam_img.jpg")
+
+st.markdown("spam messages are unwanted or fraudulent communication that clutter inboxes and may pose security risks. " \
+"To address this, spam detection systems use machine learning and " \
+"natural language processing techniques to classify messages as spam or ham(legitimate)." \
+"This Project aims to built an efficient spam detection model that can automatically filter messages," \
+"improve communication security, and enhance user experience.")
